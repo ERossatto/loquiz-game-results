0f1fadfc222e04eac64d9d2a6a8323fdc10f67e4
ERossatto <ettorerossatto@gmail.com>
2022-03-27 19:52:40 -0300

    Create texts file

---
+++ src/texts.ts
@@ -0,0 +1,14 @@
+export const texts = {
+  gameResults: {
+    textTest: {
+      pt: 'O componente esta funcionando',
+      en: 'The component works',
+    },
+  },
+  aboutProject: {
+    textTest: {
+      pt: 'O componente esta funcionando',
+      en: 'The component works',
+    },
+  },
+};
