afce8f6089cee01f1ed1a15d1b07fe952b8ae0e9
ERossatto <ettorerossatto@gmail.com>
2022-03-27 17:53:13 -0300

    Create content module

--- src/loquiz/loquiz-routing.module.ts
+++ src/loquiz/loquiz-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TestScreenComponent } from 'src/content/test-screen/test-screen.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: TestScreenComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
