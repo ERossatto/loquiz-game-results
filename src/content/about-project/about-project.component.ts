4c6fa7931c2b1702f070ad9fae9c92597c5f3139
ERossatto <ettorerossatto@gmail.com>
2022-03-27 18:56:52 -0300

    Create about-project component

---
+++ src/content/about-project/about-project.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'loquiz-about-project',
+  templateUrl: './about-project.component.html',
+  styleUrls: ['./about-project.component.scss']
+})
+export class AboutProjectComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/content/content.module.ts
+++ src/content/content.module.ts
@@ -1,13 +1,24 @@
+// Angular
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+// Modules
+import { MaterialModule } from 'src/shared/material/material.module';
+
+// Components
 import { TestScreenComponent } from './test-screen/test-screen.component';
+import { GameResultsComponent } from './game-results/game-results.component';
+import { AboutProjectComponent } from './about-project/about-project.component';
 
 @NgModule({
   declarations: [
-    TestScreenComponent
+    TestScreenComponent,
+    GameResultsComponent,
+    AboutProjectComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule,
   ]
 })
 export class ContentModule { }
