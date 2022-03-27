9617f008dcb213f28e3732882da1361febbb2fb1
ERossatto <ettorerossatto@gmail.com>
2022-03-27 18:29:55 -0300

    Standardization imports, comma and semicolon

--- src/loquiz/menu/menu.module.ts
+++ src/loquiz/menu/menu.module.ts
@@ -1,10 +1,13 @@
+// Angular
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
+// Components
 import { MenuDashComponent } from './menu-dash/menu-dash.component';
 import { MenuToggleComponent } from './menu-toggle/menu-toggle.component';
 import { MenuComponent } from './menu.component';
+
 
 @NgModule({
   declarations: [
@@ -17,7 +20,7 @@ import { MenuComponent } from './menu.component';
     RouterModule,
   ],
   exports: [
-    MenuComponent
+    MenuComponent,
   ],
 })
-export class MenuModule { }
+export class MenuModule { };
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
+// Angular
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
+// Modules
 import { LoquizModule } from './loquiz/loquiz.module';
+
+// Utils
 import { environment } from './environments/environment';
 
-if (environment.production) {
-  enableProdMode();
-}
+if (environment.production) enableProdMode();
 
-platformBrowserDynamic().bootstrapModule(LoquizModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+.bootstrapModule(LoquizModule)
+.catch(err => console.error(err));
