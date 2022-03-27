3f21d367b96a8bec84b0153ec36cd11db679bd2f
ERossatto <ettorerossatto@gmail.com>
2022-03-27 19:26:24 -0300

    Delete test-screen component

--- src/loquiz/content/content.module.ts
+++ src/loquiz/content/content.module.ts
@@ -7,13 +7,11 @@ import { MaterialModule } from 'src/shared/material/material.module';
 import { ContentRoutingModule } from './content.routing.module';
 
 // Components
-import { TestScreenComponent } from './test-screen/test-screen.component';
 import { GameResultsComponent } from './game-results/game-results.component';
 import { AboutProjectComponent } from './about-project/about-project.component';
 
 @NgModule({
   declarations: [
-    TestScreenComponent,
     GameResultsComponent,
     AboutProjectComponent,
   ],
