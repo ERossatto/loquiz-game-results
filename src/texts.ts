e196075bf58affc97414d345036a89c94429fb6f
ERossatto <ettorerossatto@gmail.com>
2022-03-27 20:28:12 -0300

    Add menu-dash texts

--- src/texts.ts
+++ src/texts.ts
@@ -1,4 +1,16 @@
 export const texts = {
+  menuDash: {
+    optionList: {
+      gameResults: {
+        pt: 'resultado dos jogos',
+        en: 'game results',
+      },
+      aboutProject: {
+        pt: 'sobre o projeto',
+        en: 'about project',
+      },
+    },
+  },
   gameResults: {
     textTest: {
       pt: 'O componente esta funcionando',
