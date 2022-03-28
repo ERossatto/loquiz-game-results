1eaef429bfc956d64bc787db3ea054cd5ad38e66
ERossatto <ettorerossatto@gmail.com>
2022-03-28 01:29:11 -0300

    Add loquiz api data into environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  loquizApi: {
+    url: 'https://api.loquiz.com/v3',
+    gamesRoute: '/games',
+    gameIdParam: (id: string): string => `/${id}`,
+    resultsIdParam: (id: string): string => `/results/${id}/teams`,
+    GAME_ID_DEFAULT: 'FK5TS3QAB',
+    API_KEY: 'ApiKey-v1 d64642249a14413ac83fdf40b57192b7be1bfb715a3e74519e279b3ee5eaf426',
+  },
 };
